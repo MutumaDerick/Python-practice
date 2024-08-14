e91d374b097b095f9d71441a7aec0eee27305e58
MutumaDerick <pdmutuma@gmail.com>
2024-08-14 09:24:42 +0300

    Python into classes

---
+++ intro.py
@@ -0,0 +1,23 @@
+#Let's store a favorite number
+favourite_number = 7
+print(f"My favourite number is {favourite_number}.")
+
+#String variable
+greeting = "Hello, World!"
+print(greeting)
+
+#Boolean
+is_raining = True
+print(f"It is raining in Nairobi: {is_raining}")
+
+#control flow: if, elif, else
+if is_raining:
+    print("You need an umbrella.")
+
+temperature = 30
+if temperature > 28:
+    print("It's a hot day.")    
+elif temperature < 18:
+        print("It's a cold day.")
+else:
+        print("It's neither hot nor cold.")
