2aa5fb5a376018b6b1c5580564b5c2d53ff8265f
MutumaDerick <pdmutuma@gmail.com>
2024-08-21 15:50:25 +0300

    Data structures and algorithms class

---
+++ stack.py
@@ -0,0 +1,15 @@
+# Stack: Stack is a linear data structure which follows a particular order in which the operations are performed.
+#  The order may be LIFO(Last In First Out) or FILO(First In Last Out).
+#  There are many real-life examples of a stack. Consider an example of plates stacked over one another in the canteen.
+
+
+stack = []
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+print(stack)
+
+print(stack.pop())
