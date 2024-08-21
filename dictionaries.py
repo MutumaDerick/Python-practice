192d0906b44074c951baefaac93b4c4662776bb2
MutumaDerick <pdmutuma@gmail.com>
2024-08-21 15:29:02 +0300

    Lists And Tuples lesson

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+# dictionaries: dictionaries are used to store data values in key:value pairs.
+my_countries = {"Kenya": "Nairobi", "Uganda": "Kampala", "Tanzania": "Dodoma", "Rwanda": "Kigali", "Burundi": "Bujumbura"}
+
+print(my_countries)
+print(my_countries["Kenya"])
---
+++ list.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 6, 9, 12, 15, 20, 25, "Hello world", True]
+print(numbers)
+print(numbers[3])
+print(numbers[-3])
+
+
+# printing out a range 
+print(numbers[2:10])
+
+# appending a list : aading items to a list
+numbers.append(993)
+
+print(numbers)
+
+# Tuple : tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+my_tuple = (2, 3, 5, 7, 9)
+
+print(my_tuple)
+print(my_tuple[3])
+
