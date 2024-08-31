fcea4ef4875e66b37ab2bde5e675b991782c2c87
MutumaDerick <pdmutuma@gmail.com>
2024-08-31 23:44:49 +0300

    Week 3 lesson 3

--- functions.py
+++ functions.py
@@ -3,4 +3,15 @@ def add_nums():
     print(2+13)
 
 # calling the function
-    
+def add_nums():
+    print(2+13)
+add_nums()
+
+# function arguements/parameters
+def add_nums(num1, num2):
+    answer = num1 + num2
+    print(num1 + num2)
+
+# assign function call to a variable
+total = add_nums(2, 13)  
+print("Total:", total)  
